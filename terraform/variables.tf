7e5a3921f415edd8a79c6f84899144d83eda0576
Jeroen Herijgers <jeroen_herijgers@hotmail.com>
2026-07-02 14:08:27 +0200

    feat: add AKS deployment with Terraform provisioning and destroy pipeline

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-aks-demo"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "aks-demo-jh"
+}
+
+variable "node_count" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "node_size" {
+  description = "VM size for worker nodes"
+  type        = string
+  default     = "Standard_B2s"
+}
